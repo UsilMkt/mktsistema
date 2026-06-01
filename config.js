93121f6c8d03c927b57728329f2b5bc285bdb627
UsilMkt <faviod629@gmail.com>
2026-05-31 23:37:27 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.USIL_SUPABASE = {
+  url: "https://tywefudmonflvjcyzgyc.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5d2VmdWRtb25mbHZqY3l6Z3ljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyNzQwODIsImV4cCI6MjA5NTg1MDA4Mn0.uSsulk9WnhRxdppgrAVVNE39krGmai5Jjd46sEiUlH8"
+};
